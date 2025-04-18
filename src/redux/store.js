337afb03b0eabbcf88255de72e793ee5b8926492
olanikegloria <olanikegloria2020@gmail.com>
2025-04-18 16:44:20 +0100

    connected the details page to redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
 import eventsReducer from "./eventsSlice"
-import eventDetailsReducer from "./eventDetailsSlice"
 
 export const store = configureStore({
   reducer: {
     events: eventsReducer,
-    eventDetails: eventDetailsReducer,
   },
 })
