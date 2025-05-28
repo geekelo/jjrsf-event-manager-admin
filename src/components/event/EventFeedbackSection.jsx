71b39035c52ea57d4787dd5c7d77555da1a713e1
nicojoy-gif <orjionyinyechi080@gmail.com>
2025-05-28 12:40:11 +0100

    updated feedback count

--- src/components/event/EventFeedbackSection.jsx
+++ src/components/event/EventFeedbackSection.jsx
@@ -7,14 +7,29 @@ import {
   faArrowRight,
 
 } from "@fortawesome/free-solid-svg-icons"
-
+import { useDispatch, useSelector } from "react-redux"
+import { fetchFeedbacks } from "../../redux/feedbackSlice"
+import { useEffect } from "react"
 const EventFeedbackSection = ({ eventId, eventName }) => {
+  const dispatch = useDispatch()
   const navigate = useNavigate()
- 
-  
+
+  // Get feedbacks and loading state from redux
+  const { allFeedbacks, loading } = useSelector(state => state.feedback)
+
+  useEffect(() => {
+    if (eventId) {
+      dispatch(fetchFeedbacks(eventId))
+    }
+  }, [dispatch, eventId])
+
   const navigateToFeedback = () => {
     navigate(`/events/${eventId}/feedback`)
   }
+
+  // Calculate count safely
+  const feedbackCount = allFeedbacks ? allFeedbacks.length : 0
+
   return (
     <section className="feedback-section-card">
       <div className="section-header">
@@ -23,19 +38,18 @@ const EventFeedbackSection = ({ eventId, eventName }) => {
         </h2>
       </div>
 
-        <div className="feedback-action-container">
-          <button className="manage-streams-button " onClick={navigateToFeedback}>
-            <span className="button-text">View All Feedback</span>
-            <span className="button-icon">
-              <FontAwesomeIcon icon={faArrowRight} />
-            </span>
-          </button>
-        </div>
-  
+      <div className="feedback-action-container">
+        <button className="manage-streams-button" onClick={navigateToFeedback} disabled={loading}>
+          <span className="button-text">
+            View All Feedback {loading ? "(Loading...)" : `(${feedbackCount})`}
+          </span>
+          <span className="button-icon">
+            <FontAwesomeIcon icon={faArrowRight} />
+          </span>
+        </button>
+      </div>
     </section>
   )
 }
-
-
 
 export default EventFeedbackSection
